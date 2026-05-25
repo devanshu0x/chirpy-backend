6919c335de273f601b28e5a595a9505675f8cbda
devanshu0x <devanshudotdev@gmail.com>
2026-05-25 18:51:33 +0530

    add a fileserver for assets and a health route

--- main.go
+++ main.go
@@ -7,10 +7,17 @@ import (
 
 func  main(){
 	mux:= http.NewServeMux()
+	mux.Handle("/app/",http.StripPrefix("/app",http.FileServer(http.Dir("."))))
 	server:=http.Server{
 		Handler: mux,
 		Addr: ":8080",
 	}
+	mux.HandleFunc("/healthz", func (w http.ResponseWriter, req *http.Request){
+		w.Header().Add("content-type","text/plain; charset=utf-8")
+		w.WriteHeader(200)
+
+		w.Write([]byte("OK"))
+	})
 	err:=server.ListenAndServe()
 	if err!=nil{
 		fmt.Printf("Error in starting sever: %v",err)
